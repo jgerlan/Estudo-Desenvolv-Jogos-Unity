8ebb41782dcc24bcd0d8187c5ae56fa6e8897bfc
jgerlan <jgerlan@gmail.com>
2018-06-17 11:30:58 -0300

    Adiciona mini projeto PlataformaSimples

---
+++ PlataformaSimples/Assets/Scripts/MovimentacaoCuboScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimentacaoCuboScript : MonoBehaviour {
+
+	float velocidade;
+	float forcaPulo;
+	bool possoPular;
+
+	// Use this for initialization
+	void Start () {
+		velocidade = 0.1f;
+		forcaPulo = 300.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.RightArrow)) {
+			this.gameObject.transform.Translate (Vector3.right * velocidade);
+		} else if (Input.GetKey(KeyCode.LeftArrow)) {
+			this.gameObject.transform.Translate (Vector3.right * -velocidade);
+		}
+
+		if (Input.GetKeyDown(KeyCode.Space) && possoPular) {
+			this.gameObject.GetComponent<Rigidbody>().AddForce (Vector3.up * forcaPulo);
+		}
+
+	}
+
+	void OnCollisionStay(Collision col) {
+		if (col.gameObject.tag.Equals ("plataforma") || col.gameObject.tag.Equals ("chao")) {
+			//Debug.Log ("Colisao");
+			possoPular = true;
+		}
+	}
+
+	void OnCollisionExit(Collision col) {
+		if (col.gameObject.tag.Equals ("plataforma") || col.gameObject.tag.Equals ("chao")) {
+			//Debug.Log ("Colisao");
+			possoPular = false;
+		}
+	}
+}
---
+++ PlataformaSimples/Assets/Scripts/PlataformaScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlataformaScript : MonoBehaviour {
+
+	float tempo;
+
+	// Use this for initialization
+	void Start () {
+		tempo = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//float i;
+		if (Time.time - tempo > 2.0f){
+			for(float i=0.0f; i<8.0f; i++){
+				Debug.Log (i);
+				this.gameObject.transform.position = new Vector3 (i, 3.0f, -4.0f);
+			}
+		}
+	}
+}
