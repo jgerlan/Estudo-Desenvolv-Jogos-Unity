fe7f34f16b2a4efb3c18ee40e6d00f403c12e165
jgerlan <jgerlan@gmail.com>
2018-06-17 16:59:51 -0300

    [#005] Adiciona mini projeto projeteis exemplo

---
+++ Projeteis_exemplo/Assets/Scripts/ProjetilBehaviourScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjetilBehaviourScript : MonoBehaviour {
+
+	public GameObject projetilPrefab;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			//atira
+			GameObject go = Instantiate(projetilPrefab) as GameObject;
+			go.transform.position = this.gameObject.transform.position;
+			go.transform.rotation = this.gameObject.transform.rotation;
+			go.transform.Translate (Vector3.forward * 1.0f);
+			go.GetComponent<Rigidbody> ().AddForce (go.transform.forward * 1000.0f);
+		}
+
+	}
+}
