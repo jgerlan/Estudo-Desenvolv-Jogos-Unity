8e25c8cebda16d64447bcf9595cd5d2a1c1b940e
jgerlan <jgerlan@gmail.com>
2018-06-17 16:37:36 -0300

    [#004] Adiciona mini projeto troca cenas

---
+++ TrocaCenas/Assets/Scripts/MovimentacaoPlayerScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimentacaoPlayerScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey (KeyCode.RightArrow)){
+			this.transform.Rotate (Vector3.up * 0.5f);
+		} else if(Input.GetKey (KeyCode.LeftArrow)){
+			this.transform.Rotate (Vector3.up * -0.5f);
+		}
+	}
+}
---
+++ TrocaCenas/Assets/Scripts/VoltarScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class VoltarScript : MonoBehaviour {
+
+	public void voltarMenuPrincipal(){
+		SceneManager.LoadScene ("Menu");
+	}
+
+	public void irCreditos() {
+		SceneManager.LoadScene ("Créditos");
+	}
+}
